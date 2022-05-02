4a8892d4fb5eb0873f966ea4af4caf4c9143fe77
NodyDuoc <ale.lara@duocuc.cl>
2022-05-02 18:16:06 -0400

    Registro de usuario

--- Recetas_de_Sudamerica/js/jquery/RegUser.js
+++ Recetas_de_Sudamerica/js/jquery/RegUser.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         var nombre = $("#nomUser").val();
         var ape = $("#apeUser").val();
         var nick = $("#nickUserName").val();
+        var mail = $("#email")
         var pass1 = $("#password1").val();
         var pass2 = $("#password2").val();
 
@@ -17,39 +18,58 @@ $(document).ready(function () {
             entrar = false;
         }
 
-        if(!ape==""){
+        if (nombre.length > 50) {
+            mensajeMostrar += "El nombre puede contener un maximo de 50 caracteres<br>";
+            entrar = false;
+        }
+
+
+        if (!ape == "") {
 
             if (!esMayuscula(ape.charAt(0))) {
                 mensajeMostrar += "La primera letra del apellido debe ser mayuscula<br>";
                 entrar = false;
             }
-
+            if (ape.length > 50) {
+                mensajeMostrar += "El apellido puede contener un maximo de 50 caracteres<br>";
+                entrar = false;
+            }
         }
 
-        var UltimoDigito = nick.charAt(nick.length-1);
-        if(isNaN(UltimoDigito)){
+        var UltimoDigito = nick.charAt(nick.length - 1);
+        if (isNaN(UltimoDigito)) {
             mensajeMostrar += "El ultimo digito del nombre de usuario debe ser un numero<br>";
             entrar = false;
         }
 
         if (nick.length < 4 || nick.length > 60) {
-            mensajeMostrar += "La longitud del nombre de usuario no es adecuada, debe tener entre 4 y 60 caracteres<br>";
+            mensajeMostrar += "El nombre de usuario debe tener entre 4 y 60 caracteres<br>";
             entrar = false;
         }
-        
 
-        if (pass1 != pass2){
+        if (mail.length > 70) {
+            mensajeMostrar += "El correo no puede contener mas de 70 digitos<br>";
+            entrar = false;
+        }
+
+        if ((pass1.length > 60 || pass1.length < 4) || (pass2.length > 60 || pass2.length < 4)) {
+            mensajeMostrar += "La contraseñas no pueden contener mas de 60 o menos de 4 digitos<br>";
+            entrar = false;
+        }
+
+        if (pass1 != pass2) {
             mensajeMostrar += "Las contraseñas no coinciden<br>";
             entrar = false;
         }
 
 
-        if(entrar){
+
+        if (entrar) {
             $("#mensajeReg").html("Su cuenta ha sido registrada");
-            
+
 
         }
-        else{
+        else {
             $("#mensajeReg").html(mensajeMostrar);
         }
 
