fda6554039aa2d09441ac82623025e632c5440e6
NodyDuoc <ale.lara@duocuc.cl>
2022-05-03 21:08:06 -0400

    Js registro

--- Recetas_de_Sudamerica/js/jquery/RegUser.js
+++ Recetas_de_Sudamerica/js/jquery/RegUser.js
@@ -8,18 +8,21 @@ $(document).ready(function () {
         var mail = $("#email")
         var pass1 = $("#password1").val();
         var pass2 = $("#password2").val();
+        var UltimoDigito = nick.charAt(nick.length - 1);
+        var UltimoDigiPass1 = pass1.charAt(pass1.length - 1);
+        var UltimoDigiPass2 = pass2.charAt(pass2.length - 1);
 
         let mensajeMostrar = "";
         let entrar = true;
 
 
         if (!esMayuscula(nombre.charAt(0))) {
-            mensajeMostrar += "La primera letra del nombre debe ser mayuscula<br>";
+            mensajeMostrar += "La primera letra del nombre debe ser mayúscula<br>";
             entrar = false;
         }
 
         if (nombre.length > 50) {
-            mensajeMostrar += "El nombre puede contener un maximo de 50 caracteres<br>";
+            mensajeMostrar += "El nombre puede contener un máximo de 50 caracteres<br>";
             entrar = false;
         }
 
@@ -27,11 +30,11 @@ $(document).ready(function () {
         if (!ape == "") {
 
             if (!esMayuscula(ape.charAt(0))) {
-                mensajeMostrar += "La primera letra del apellido debe ser mayuscula<br>";
+                mensajeMostrar += "La primera letra del apellido debe ser mayúscula<br>";
                 entrar = false;
             }
             if (ape.length > 50) {
-                mensajeMostrar += "El apellido puede contener un maximo de 50 caracteres<br>";
+                mensajeMostrar += "El apellido puede contener un máximo de 50 caracteres<br>";
                 entrar = false;
             }
         }
@@ -53,9 +56,20 @@ $(document).ready(function () {
         }
 
         if ((pass1.length > 60 || pass1.length < 4) || (pass2.length > 60 || pass2.length < 4)) {
-            mensajeMostrar += "La contraseñas no pueden contener mas de 60 o menos de 4 digitos<br>";
+            mensajeMostrar += "La contraseñas debe tener entre 4 y 60 caracteres<br>";
             entrar = false;
         }
+
+        if (!esMayuscula(pass1.charAt(0)) || !esMayuscula(pass2.charAt(0))) {
+            mensajeMostrar += "La primera letra de la contraseña debe ser mayúscula<br>";
+            entrar = false;
+        }
+
+        if ((isNaN(UltimoDigiPass1)) || (isNaN(UltimoDigiPass2))) {
+            mensajeMostrar += "El ultimo digito de la contraseña debe ser un numero<br>";
+            entrar = false;
+        }
+
 
         if (pass1 != pass2) {
             mensajeMostrar += "Las contraseñas no coinciden<br>";
