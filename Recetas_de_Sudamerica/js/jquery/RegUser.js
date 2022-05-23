cee35433e184c6a84817625bc3484473da8f99da
NodyDuoc <ale.lara@duocuc.cl>
2022-05-23 19:11:22 -0400

    Registro de usuario

--- Recetas_de_Sudamerica/js/jquery/RegUser.js
+++ Recetas_de_Sudamerica/js/jquery/RegUser.js
@@ -8,14 +8,12 @@ $(document).ready(function () {
         var mail = $("#email")
         var pass1 = $("#password1").val();
         var pass2 = $("#password2").val();
-        var UltimoDigito = nick.charAt(nick.length - 1);
-        var UltimoDigiPass1 = pass1.charAt(pass1.length - 1);
-        var UltimoDigiPass2 = pass2.charAt(pass2.length - 1);
+      
 
         let mensajeMostrar = "";
         let entrar = true;
 
-
+        
         if (!esMayuscula(nombre.charAt(0))) {
             mensajeMostrar += "La primera letra del nombre debe ser mayúscula<br>";
             entrar = false;
@@ -39,9 +37,9 @@ $(document).ready(function () {
             }
         }
 
-        var UltimoDigito = nick.charAt(nick.length - 1);
-        if (isNaN(UltimoDigito)) {
-            mensajeMostrar += "El último dígito del nombre de usuario debe ser un número<br>";
+        
+        if (!tieneNumero(nick)) {
+            mensajeMostrar += "Un dígito del nombre de usuario debe ser un número<br>";
             entrar = false;
         }
 
@@ -57,16 +55,16 @@ $(document).ready(function () {
 
         if ((pass1.length > 60 || pass1.length < 4) || (pass2.length > 60 || pass2.length < 4)) {
             mensajeMostrar += "La contraseñas debe tener entre 4 y 60 caracteres<br>";
+            entrar = false; 
+        }
+
+        if (!(isUpper(pass1) || isUpper(pass2))) {
+            mensajeMostrar += "Una letra de la contraseña debe ser mayúscula<br>";
             entrar = false;
         }
 
-        if (!esMayuscula(pass1.charAt(0)) || !esMayuscula(pass2.charAt(0))) {
-            mensajeMostrar += "La primera letra de la contraseña debe ser mayúscula<br>";
-            entrar = false;
-        }
-
-        if ((isNaN(UltimoDigiPass1)) || (isNaN(UltimoDigiPass2))) {
-            mensajeMostrar += "El último dígito de la contraseña debe ser un número<br>";
+        if (!tieneNumero(pass1) || !tieneNumero(pass2)) {
+            mensajeMostrar += "Un dígito de la contraseña debe ser un número<br>";
             entrar = false;
         }
 
@@ -75,21 +73,23 @@ $(document).ready(function () {
             mensajeMostrar += "Las contraseñas no coinciden<br>";
             entrar = false;
         }
-
-
-
         if (entrar) {
             $("#mensajeReg").html("Registro exitoso");
-
-
         }
         else {
             $("#mensajeReg").html(mensajeMostrar);
         }
-
     });
 })
 
 function esMayuscula(letra) {
     return letra == letra.toUpperCase();
 };
+
+function isUpper(str) {
+    return /[A-Z]/.test(str);
+}
+
+function tieneNumero(numero) {
+    return /\d/.test(numero);
+}
